4d82c3fe86396db2117ae31b907f52b6eb110f33
Caro <carolin.stellmacher@haw-hamburg.de>
2015-06-28 19:36:10 +0200

    merge canny into project

--- ShapeScan/ShapeScan/main.cpp
+++ ShapeScan/ShapeScan/main.cpp
@@ -1,41 +1,71 @@
-#include <opencv2\opencv.hpp>
+//
+//  canny.cpp
+//  IMPLecture1
+//
+//  Created by Nielab Schahrochie on 17.04.15.
+//  Copyright (c) 2015 haw. All rights reserved.
+//
+
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
-using namespace std;
+
 using namespace cv;
 
-void firstTask() {
-	Mat logoImg = imread("logo.png");
+Mat src_gray;
+Mat dst, detected_edges;
 
-	for (int x = 0; x < logoImg.rows; x++) {
-		for (int y = 0; y < logoImg.cols; y++) {
-			//Vec3b intensity = image.at<Vec3b>(y, x);
-			if (logoImg.at<Vec3b>(x, y) == Vec3b(0, 0, 0)) {
-				logoImg.at<Vec3b>(x, y) = Vec3b(0, 0, 255);
-			}
-		}
-	}
+//
+// globale Variablen
+//
 
-	namedWindow("Window");
-	imshow("Window", logoImg);
+int edgeThresh = 1;
 
-	waitKey(0);
+//Schwellenwert
+int lowThreshold = 150;
+
+//Verhaeltnis 3:1
+int ratio = 3;
+
+//Kern=3 fuer Sobel
+int kernel_size = 3;
+
+//Ergebnis
+const char* ergebnis = "Canny";
+
+//
+// CannyThreshold
+//
+
+static void CannyThreshold(){
+	// Sobel mit Kern 3
+	blur(src_gray, detected_edges, Size(3, 3));
+
+	// Canny detector
+	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
+
+	// schwarzes Bild
+	dst = Scalar::all(0);
+
+	//Kanten in schwarzes Bild kopieren
+	src_gray.copyTo(dst, detected_edges);
+	imshow(ergebnis, dst);
 }
 
-void secondTask() {
-	Mat lenaImg = imread("lena.jpg");
+int main(){
+	/// Load an image
+	src_gray = imread("fanta.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
-	namedWindow("LenaWindow");
-	imshow("LenaWindow", lenaImg);
+	// Matrix mit selben Maﬂen wie src_gray
+	dst.create(src_gray.size(), src_gray.type());
+
+	// Anzeigefenster
+	namedWindow(ergebnis, WINDOW_AUTOSIZE);
+
+	// Anzeigen
+	CannyThreshold(/*0, 0*/);
+
 
 	waitKey(0);
-}
-
-void main()
-{
-	//firstTask();
-	secondTask();
-
-
-
+	return 0;
 }
