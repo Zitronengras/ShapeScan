ce86b139d70dfd96cae11626653620ed37b6d23c
S0NNI <so_wi@gmx.de>
2015-07-05 14:47:12 +0200

    add Camera

--- ShapeScan/ShapeScan/main.cpp
+++ ShapeScan/ShapeScan/main.cpp
@@ -7,12 +7,52 @@
 #include "cannyEdgeDet.h"
 #include "houghLine.h"
 #include "houghCircle.h"
+#include "Camera.h"
 
 using namespace cv;
 using namespace std;
 
+Mat frame;
 
-void main(){
+int process(VideoCapture& cap){
+	int n = 0;
+	char filename[100];
+	string windowName = "Video | esc to quit";
+	cout << "Press space to save a picture" << endl;
+
+	namedWindow(windowName, CV_WINDOW_KEEPRATIO);
+
+	for (;;){
+		cap >> frame;			// get a new Frame from Camera
+		if (frame.empty())
+			break;
+
+		imshow(windowName, frame);
+
+		char key = (char)waitKey(5);
+		switch (key){
+		case 27:		// esc -> Quit
+			return 0;
+		case ' ':		// space -> Save Image 
+			sprintf(filename, "Picture%.3d.jpg", n++);		//Name: Picture00n.jpg
+			imwrite(filename, frame);
+			cout << "Saved" << filename << endl;
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
+
+}
+
+
+int main(){
+
+	VideoCapture cap(0);		// open the default camera
+	if (!cap.isOpened())		// check if we succeded
+		return -1;
+
 
 	CannyEdgeDet cannyEdgeDet;
 	HoughLine hougLine;
@@ -34,6 +74,8 @@ void main(){
 
 	namedWindow("ShapeScanCircle", WINDOW_AUTOSIZE);
 	imshow("ShapeScanCircle", houghCircleImg);
-
+	
+	return process(cap);
 	waitKey(0);
+
 }
