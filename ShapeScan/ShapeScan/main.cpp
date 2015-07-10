293ae080befb01d30d0d5ddd054a3fde74d8bace
Magnifica <magnifica92@hotmail.de>
2015-07-10 11:44:50 +0200

    changes

--- ShapeScan/ShapeScan/main.cpp
+++ ShapeScan/ShapeScan/main.cpp
@@ -21,27 +21,6 @@ int main(){
 	if (!cap.isOpened())		// check if we succeded
 	return -1;
 
-	CannyEdgeDet cannyEdgeDet;
-	HoughLine hougLine;
-	HoughCircle houghCircle;
-	//load image
-	Mat image = imread("card.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-
-	//store image of canny
-	Mat cannyImg = cannyEdgeDet.runCanny(image);
-
-	//store image of houghLine
-	Mat houghLineImg = hougLine.detectLines(cannyImg);
-
-	namedWindow("ShapeScanLine", WINDOW_AUTOSIZE);
-	imshow("ShapeScanLine", houghLineImg);
-
-	//store image of houghCircle
-	Mat houghCircleImg = houghCircle.detectCircle(cannyImg);
-
-	namedWindow("ShapeScanCircle", WINDOW_AUTOSIZE);
-	imshow("ShapeScanCircle", houghCircleImg);
-
 	return ca.process(cap);
 	waitKey(0);
 
